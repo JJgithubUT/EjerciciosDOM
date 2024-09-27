b133bedeb9756aeac09154c6b3ab26432deca908
JuanJavier <juanjcbreton@gmail.com>
2024-09-27 16:52:29 -0600

    02 Actualización de btns eliminar y guardar en casa (vie-27-09-24)

--- todoapp/app.js
+++ todoapp/app.js
@@ -4,19 +4,53 @@ const divTareas = document.getElementById('tareas');
 
 const addTarea = () => {
     const tarea = inputTarea.value;
-    divTareas.appendChild(createTarea(tarea));
-    inputTarea.value = '';
+    if (tarea.trim() !== "") {
+        divTareas.appendChild(createTarea(tarea));
+        inputTarea.value = '';
+    }
 }
 
 const createTarea = (tarea) => {
     const div = document.createElement('div');
     const p = document.createElement('p');
-    const button = document.createElement('button');
+    const buttonEliminar = document.createElement('button');
+    const buttonEditar = document.createElement('button');
+
     p.innerText = tarea;
-    button.innerText = 'Eliminar';
+    buttonEliminar.innerText = 'Eliminar';
+    buttonEditar.innerText = 'Editar';
+
+    buttonEliminar.addEventListener('click', () => {
+        div.remove();
+    });
+
+    buttonEditar.addEventListener('click', () => {
+        editTarea(div, p);
+    });
+
     div.appendChild(p);
-    div.appendChild(button);
+    div.appendChild(buttonEditar);
+    div.appendChild(buttonEliminar);
     return div;
+}
+
+const editTarea = (div, p) => {
+    const inputEdit = document.createElement('input');
+    inputEdit.type = 'text';
+    inputEdit.value = p.innerText;
+
+    const buttonGuardar = document.createElement('button');
+    buttonGuardar.innerText = 'Guardar';
+
+    div.replaceChild(inputEdit, p);
+    const buttonEditar = div.querySelector('button:nth-child(2)');
+    div.replaceChild(buttonGuardar, buttonEditar);
+
+    buttonGuardar.addEventListener('click', () => {
+        p.innerText = inputEdit.value;
+        div.replaceChild(p, inputEdit);
+        div.replaceChild(buttonEditar, buttonGuardar);
+    });
 }
 
 btnAdd.addEventListener('click', addTarea);
