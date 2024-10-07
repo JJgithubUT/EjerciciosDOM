abf2c45c15ccaa8f7aafe87476d1c8e82c8d8bc7
JuanJavier <juanjcbreton@gmail.com>
2024-10-07 16:21:59 -0600

    07 Modificación final atributos img 7-10-24

--- pptimg/pptimg.js
+++ pptimg/pptimg.js
@@ -2,13 +2,17 @@ const images = document.querySelectorAll('.images img');
 const imgUser = document.querySelector('#choiceUser');
 const imgCompu = document.querySelector('#choiceCompu');
 const options = ['rock', 'paper', 'scissors'];
+const boardWinner = document.querySelector('#resultadosGanador');
+const boardScores = document.querySelector('#puntosTablero');
+let userScore = 0;
+let compuScore = 0;
 
 const getChoiceUser = (e) => {
     const image = e.target;
     const choiceUser = image.getAttribute('data-id');
     const imageSrc = image.src;
     // pintar imagen de choices
-    imgUser.src = imageSrc; // Actualiza la imagen del usuario
+    imgUser.src = imageSrc; // actualiza la img de la chonpu
     // llamar la función play
     play(choiceUser, e);
 };
@@ -18,21 +22,25 @@ const play = (choiceUser, e) => {
     const tiroCompu = options[Math.floor(Math.random() * options.length)];
     images.forEach(image => {
         if (image.getAttribute('data-id') === tiroCompu) {
-            imgCompu.src = image.src; // Actualiza la imagen de la computadora
+            imgCompu.src = image.src;
         }
     });
 
     if (choiceUser === tiroCompu) {
-        alert("EMPATE");
+        boardWinner.innerText = 'Empate';
     } else if (
         (choiceUser === options[0] && tiroCompu === options[2]) ||
         (choiceUser === options[2] && tiroCompu === options[1]) ||
         (choiceUser === options[1] && tiroCompu === options[0])
     ) {
-        alert("Ganaste");
+        boardWinner.innerText = 'Ganaste';
+        userScore++;
     } else {
-        alert("Perdiste");
+        boardWinner.innerText = 'Perdiste';
+        compuScore++;
     }
+
+    boardScores.innerText = `Jugador ${ userScore } || ${ compuScore } IA`;
     
 };
 
