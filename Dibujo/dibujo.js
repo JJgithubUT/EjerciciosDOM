9ffdf06a35356abc599ae632d61e8f1e64946b67
JuanJavier <juanjcbreton@gmail.com>
2024-10-11 21:30:53 -0600

    9.7 Dibujo actualizable(Frankenstein y Cara 11-10-24)

--- Dibujo/dibujo.js
+++ Dibujo/dibujo.js
@@ -1,9 +1,34 @@
 const lienzo = document.querySelector('#lienzo');
 const ctx = lienzo.getContext('2d');
+const h1Boton = document.querySelector('#h1Boton');
+let cambiador = 0;
 
-//ctx.fillRect(0,0,400,400);
+h1Boton.addEventListener('click', () => {
+    switch (cambiador) {
+        case 0:
+            reiniciar();
+            cuadroFrank();
+            cambiador = 1;
+            break;
+        case 1:
+            reiniciar();
+            cuadroCara();
+            cambiador = 0;
+            break;
+        default:
+            break;
+    }
+})
 
-// triangulo
+const reiniciar = () => {
+    lienzo.width = 400;
+    lienzo.height = 400;
+    ctx.fillStyle = 'rgb(46,46,46)';
+    ctx.fillRect(0, 0, 400, 400);
+}
+
+
+const cuadroFrank = () => {
 
 ctx.fillStyle = 'rgb(106,57,6)';
 ctx.roundRect(200,100,100,150,10);
@@ -53,3 +78,20 @@ ctx.moveTo(255,200);
 ctx.lineTo(275,200);
 ctx.lineTo(265,210);
 ctx.fill();
+
+}
+
+const cuadroCara = () => {
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillRect(50, 50, 100, 50);
+ctx.fillRect(250, 50, 100, 50);
+ctx.fillRect(50, 200, 50, 50);
+ctx.fillRect(100, 250, 200, 50);
+ctx.fillRect(300, 200, 50, 50);
+ctx.fillRect(150, 300, 100, 50);
+ctx.fillStyle = 'red';
+ctx.arc(200,170,50,10,Math.PI * 1, true);
+ctx.fill();
+ctx.fillRect(100, 50, 50, 50);
+ctx.fillRect(250, 50, 50, 50);
+}
